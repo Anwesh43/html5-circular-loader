f2c91b96d0e4b72857e4c081d6304979a731d747
Anwesh43 <anweshthecool0@gmail.com>
2017-05-19 21:21:23 +0530

    Adding minor fixes

--- client/CircularLoader.js
+++ client/CircularLoader.js
@@ -1,7 +1,7 @@
 class Loader {
     constructor(blockingMethod) {
         this.blockingMethod = blockingMethod
-        this.circularLoader = new CircularLoader()
+
     }
     startLoading() {
       const canvas = this.canvas
@@ -10,11 +10,12 @@ class Loader {
       context.clearRect(0,0,w,h)
       context.save()
       context.fillStyle = 'black'
-      context.globalAlpha = 0.5
+      context.globalAlpha = 0.3
       context.fillRect(0,0,w,h)
       this.circularLoader.draw(context)
       this.circularLoader.update()
       context.restore()
+      this.img.src = canvas.toDataURL()
     }
     load() {
         this.img = document.createElement('img')
@@ -22,43 +23,49 @@ class Loader {
         this.context = this.canvas.getContext('2d')
         this.canvas.width = window.innerWidth
         this.canvas.height = window.innerHeight
+        const w = window.innerWidth,h = window.innerHeight
+        this.circularLoader = new CircularLoader(w/2,h/2,Math.min(w,h)/10)
         document.body.appendChild(this.img)
+        const interval  =  setInterval(()=>{
+            this.startLoading()
+        },100)
+    }
+}
+class CircularLoader {
+    constructor(x,y,r,n) {
+        this.x = x
+        this.y = y
+        this.r = r
+        this.currIndex = 0
+        this.n = n || 8
+    }
+    draw(context) {
+        const n = this.n
+        for(var i=0;i<n;i++) {
+            context.save()
+            context.translate(this.x,this.y)
+            context.rotate(-3*Math.PI/2+i*(2*Math.PI/n));
+            context.save()
+            context.globalAlpha = 0.3
+            this.drawLine(context)
+            context.restore()
+            if(i == this.currIndex) {
+                this.drawLine(context)
+            }
+            context.restore()
+        }
+    }
+    drawLine(context) {
+        context.strokeStyle = 'white'
+        context.lineWidth = this.r/5
+        context.beginPath()
+        context.moveTo(0,-this.r/3)
+        context.lineTo(0,-this.r)
+        context.stroke()
 
     }
-    class CircularLoader {
-        constructor(x,y,r) {
-            this.x = x
-            this.y = y
-            this.r = r
-            this.currIndex = 0
-        }
-        draw(context) {
-            for(var i=0;i<6;i++) {
-                context.save()
-                context.translate(this.x,this.y)
-                context.rotate(-3*Math.PI/2+i*Math.PI/3);
-                context.save()
-                context.globalAlpha = 0.5
-                this.drawLine(context)
-                context.restore()
-                if(i == this.currIndex) {
-                    this.drawLine(context)
-                }
-                context.restore()
-            }
-        }
-        drawLine(context) {
-            context.strokeStyle = 'white'
-            context.lineWidth(r/5)
-            context.beginPath()
-            context.moveTo(0,-this.r/3)
-            context.lineTo(0,-this.r)
-            context.stroke()
-
-        }
-        update() {
-            this.currIndex++
-            this.currIndex %= 6
-        }
+    update() {
+        this.currIndex++
+        this.currIndex %= this.n
     }
 }
